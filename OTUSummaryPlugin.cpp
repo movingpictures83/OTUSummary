482b9253c5236aa2df5fff26f7d62b23158db637
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-06-25 15:40:00 -0400

    Initial commit

---
+++ OTUSummaryPlugin.cpp
@@ -0,0 +1,22 @@
+#include "PluginManager.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "OTUSummaryPlugin.h"
+
+void OTUSummaryPlugin::input(std::string file) {
+ inputfile = file;
+}
+
+void OTUSummaryPlugin::run() {
+   
+}
+
+void OTUSummaryPlugin::output(std::string file) {
+//beta_diversity.py -i filtered_otu_table.biom -m euclidean,weighted_unifrac,unweighted_unifrac -t rep_set.tre -o beta_div
+   std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; summarize_taxa.py ";
+ command += "-i "+inputfile+" -o "+file+"; cp "+file+"/* "+file+"/..";
+ std::cout << command << std::endl;
+ system(command.c_str());
+}
+
+PluginProxy<OTUSummaryPlugin> OTUSummaryPluginProxy = PluginProxy<OTUSummaryPlugin>("OTUSummary", PluginManager::getInstance());
---
+++ OTUSummaryPlugin.h
@@ -0,0 +1,22 @@
+#ifndef OTUSUMMARYPLUGIN_H
+#define OTUSUMMARYPLUGIN_H
+
+#include "Plugin.h"
+#include "PluginProxy.h"
+#include <string>
+#include <vector>
+
+class OTUSummaryPlugin : public Plugin
+{
+public: 
+ std::string toString() {return "OTUSummary";}
+ void input(std::string file);
+ void run();
+ void output(std::string file);
+
+private: 
+ std::string inputfile;
+ std::string outputfile;
+};
+
+#endif
