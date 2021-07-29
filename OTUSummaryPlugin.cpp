2cc84a2beca4b3ed217a6b6d3d0498fa1c8f24e4
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-07-29 08:36:40 -0400

    Removed Python2

--- OTUSummaryPlugin.cpp
+++ OTUSummaryPlugin.cpp
@@ -15,7 +15,7 @@ void OTUSummaryPlugin::output(std::string file) {
 //beta_diversity.py -i filtered_otu_table.biom -m euclidean,weighted_unifrac,unweighted_unifrac -t rep_set.tre -o beta_div
    //std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; summarize_taxa.py ";
    std::string command = "summarize_taxa.py ";
- command += "-i "+inputfile+"-L2,3,4,5,6,7 -o "+file+"; cp "+file+"/* "+file+"/..";
+ command += "-i "+inputfile+" -L2,3,4,5,6,7 -o "+file+"; cp "+file+"/* "+file+"/..";
  std::cout << command << std::endl;
  system(command.c_str());
 }
